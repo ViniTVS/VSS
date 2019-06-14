c35490adbc26830c2b38bb6a0e47d6c83b53a34d
Vinícius Santos <vinisantos185@gmail.com>
2019-06-14 12:24:01 -0300

    adição do código do menino-prodígio

--- códigos feitos/interface.py
+++ códigos feitos/interface.py
@@ -1,26 +1,37 @@
 # -*- coding: UTF-8 -*-
 from getch import getch
 import serial
-from serial import Serial
+# from serial import Serial
 
 SSPEED = 115200
 print ("Escreva a porta conforme comentado no código")
 porta = input("Porta conectada: ")	# é necessário que o usuário forneça a porta p/ conexão serial
-									# exemplo: '/dev/ttyS0' (é necessário o uso de aspas simples)
+									# exemplo: '/dev/ttyUSB0' (é necessário o uso de aspas simples)
 ArduinoSerial = serial.Serial(porta,SSPEED)
 #time.sleep(2)
 #print ArduinoSerial.readline()
 #ArduinoSerial.write('n')	
-string = "quero que funcione"
+string = "quero que fe"
 
 print ("Precione [p] para sair:")
+# string = raw_input("digite algo:")	
 
 while True:
     #entrada = 'n'				# defino o dado como n p/ funcionar semelhante a um nop e não pegar lixo de memória
-	entrada = getch();	# pega chars como entrada sem a necesidade de teclar enter
-
+	# pega chars como entrada sem a necesidade de teclar enter
+	string = raw_input("digite algo:")	
 	ArduinoSerial.write(string)
-	#print ("A entrada foi:"),entrada
 
-	if entrada == 'p' or entrada == 'P':		# critério p/ sair do loop
-		break
+
+	# while i < 10**90:
+	# 	i = i + 1
+	# 	if i == 100:
+	# 		ArduinoSerial.write(string)
+			
+	# break
+		# print(i)
+
+	#print ("A entrada foi:"),entrada
+	# time(500)
+	# if entrada == 'p' or entrada == 'P':		# critério p/ sair do loop
+		# break
--- códigos feitos/translation.ino
+++ códigos feitos/translation.ino
@@ -1,15 +1,49 @@
-typedef union {
-  struct {
-    uint8_t  pwm_motor_B;        // (bits 0-7)   8 bits: Valor do PWM do Motor B
-    uint8_t  pwm_motor_A;        // (bits 8-15)  8 bits: Valor do PWM do Motor A
-    uint8_t  dir_motor_B : 2,    // (bits 16-17) 2 bits: BIN1 e BIN2  da ponte H
-             dir_motor_A : 2,    // (bits 18-19) 2 bits: AIN1 e AIN2  da ponte H
-             addr        : 4;    // (bits 20-23) 4 bits não utilizados (padding)
-    uint8_t  ign2_8b;            // (bits 24-31) 8 bits não utilizados (padding)
-  } config;
-  uint32_t status = 0;           // Leitura/Escrita simuntânea do conjunto de variáveis.
-} TMotCtrl;
+//#include <Motorino.h>
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
 
+// da base:
+
+#define SSPEED        115200   // Velocidade da interface serial
+#define SERIAL_ECHO        0   // Eco da msg recebida (Prod: 0 | Debug: 1)
+
+#define TAM_BUFFER        16   // Buffer de SW para o rádio
+#define BASE_ADDRESS      00   // Base tem o endereço 7 (em octal)
+#define netw_channel     100   // Canal padrão de operação do rádio
+
+// Pinos de controle do módulo de rádio (independentes da SPI)
+
+#define RADIO_CE          A0   // Pino CE do módulo de rádio
+#define RADIO_CS          A1   // Pino CS do módulo do rádio
+#define RADIO_PWR         A2   // Liga/Desl. módulo de rádio
+
+// LEDs de sinalização.
+// Obs: Os LEDs vermelhos estão conectados a pinos com PWM.
+
+#define L1G                4   // Led 1 verde
+#define L1R                5   // Led 1 vermelho
+#define L2G                7   // Led 2 verde
+#define L2R                6   // Led 2 vermelho
+#define L3G                8   // Led 3 verde
+#define L3R                9   // Led 3 vermelho
+
+RF24 radio(RADIO_CE, RADIO_CS);  // Instância do rádio
+
+// ------------------------- declaração de typedefs -----------------------------
+
+typedef union {
+    struct {
+        uint8_t pwm_A;        // bits menos significativos
+        uint8_t pwm_B;
+        uint16_t ponte_A : 2,  
+                 ponte_B : 2,
+                 angulo  : 9,                
+                 not_used: 2,
+         ponte   : 1; // bits mais significativos
+    };
+    uint32_t hexa = 0x00000000;   // sempre que declarar uma variável controle, ela iniciará com seus valores zerados
+} mensagem;
 
 typedef union {
     struct {
@@ -22,82 +56,200 @@ typedef union {
 
 typedef union  {
     struct {
-        uint16_t  id    :4,
-                  chr   :8,
+        char      chr;
+        uint8_t   id    :4,
                   pad   :4;
         Data      data;
     } conf;
     uint32_t  stats = 0;
 } TRadioMsg;
 
-/*
-typedef union  {
-    struct {
-        uint32_t  id    :4,
-                  chr   :8,
-                  pad   :3,
-                  data1 :8,
-                  data2 :9;
-    } conf;
-    uint32_t  stats = 0;
-} TRadioMsg;
-*/
-
-TMotCtrl motor;                            //Variavel global que contra o motor.
-TRadioMsg msg;                             // Mensagem que está sendo executada.
-
-int final_time = 0;                            //tempo p/ ser percorrido x cm
-int act_time = 0;                              //tempo percorrido no momento
 
 
-     
-void calcula_tempo (void) {
-  final_time = msg.conf.data.data1 / msg.conf.data.data2;  
-}
+//  ----------------------- declaração de variáveis globais -----------------------------------
 
-uint8_t mm_to_pwm (void){
-   uint8_t saida;
-   saida = 159*msg.conf.data.data2/500;
-   return saida;
-}
+char data_input;
+mensagem data, data_exit;
+// char data_ant = 'n';
+const byte canais[2] = {0x00, 0xFF}; 
+unsigned long final_time;
 
-void translate_msg_in (void){
-    switch (msg.conf.chr){
-        case 'M':
-            
-            break;
-        case 'R':
-                    
-            break;
-        case 'S':
-            motor.config.dir_motor_A = 0b10;
-            motor.config.dir_motor_B = 0b10;
-            motor.config.pwm_motor_A = mm_to_pwm();
-            motor.config.pwm_motor_B = mm_to_pwm();
+// --------------------------- declaração de funções e afins --------------------------------
 
-            break;
-        case 'P':
-            motor.config.dir_motor_A = 0b00;
-            motor.config.dir_motor_B = 0b00;
-            break;
-        case 'W':
-                    
-            break;
-        case 'L':
-                    
-            break;
-        case 'D':
-//            motor.config. 
-            break;
-        default:
-            break;
-
-        motor.config.addr = msg.conf.id;  // transfiro o end. da msg p/ o do 'motor'
-
-//        msg.conf.dado.chave = 10;
+void translate_input(char input){
+    input = toupper(input);
+    switch (input){
+    case 'W':
+        data.ponte = 1;
+        data.ponte_A = 0b01;
+        data.ponte_B = 0b01;  
+        break;
+    
+    case 'A':
+        data.ponte = 1;
+        data.ponte_A = 0b00;
+        data.ponte_B = 0b01;
+        data.angulo = 90;
+        break;
+    
+    case 'S':
+        data.ponte = 1;
+        data.ponte_A = 0b11;
+        data.ponte_B = 0b11;
+    
+        break;
+    
+    case 'D':
+        data.ponte = 1;
+        data.ponte_A = 0b01;
+        data.ponte_B = 0b00; 
+        data.angulo = 90;
+        break;
+    
+    case 'M':
+        data.pwm_A = data.pwm_A + 10;
+        data.pwm_B = data.pwm_B + 10;
+        break;
+    
+    case 'N':
+        data.pwm_A = data.pwm_A - 10;
+        data.pwm_B = data.pwm_B - 10;    
+        break;
+    
+    default:
+        data.ponte = 0;
+        break;
     }
 }
 
-void set_motor_status( TRadioMsg state){
+void send_message(){
+    if (!radio.write( &data.hexa, sizeof(mensagem))) {
+        Serial.print("Transmissão ");
+        Serial.print(data.hexa, HEX);
+        Serial.println(" não enviada - conexão perdida");
+        //Serial.println (final_time);
 
+        // caso não consiga enviar os dados, desligos os LEDs verdes e ligo os vermelhos
+        digitalWrite(L1R, HIGH);
+        digitalWrite(L2R, HIGH);
+        digitalWrite(L3R, HIGH);
+        digitalWrite(L1G, LOW);
+        digitalWrite(L2G, LOW);
+        digitalWrite(L3G, LOW);
+    }
+    //if(radio.write( &data_saida, sizeof(char))){
+    else{ 
+        Serial.print("Sent: ");     // aviso qual dado foi enviado
+        Serial.println(data.hexa);
+
+        // ligo os LEDs verdes 2 e 3 e desligo o resto
+        digitalWrite(L1G, LOW);
+        digitalWrite(L1R, LOW);
+        digitalWrite(L2R, LOW);
+        digitalWrite(L3R, LOW);
+        digitalWrite(L2G, HIGH);
+        digitalWrite(L3G, HIGH);
+        //Serial.print (final_time);
+    }
+}
+
+// --------------------------- setup e loop do arduino ----------------------------------------
+
+void setup() {
+    Serial.begin(SSPEED);
+    Serial.println("Arduino transmissor ligado");
+
+    pinMode(L1G, OUTPUT);
+    digitalWrite (L1G, LOW);
+
+    pinMode(RADIO_PWR, OUTPUT);
+    digitalWrite(RADIO_PWR, HIGH);
+
+    radio.begin();
+    radio.setPALevel(RF24_PA_MAX);        // deixar o alcance o máx. possível por... motivos
+    radio.setDataRate(RF24_2MBPS);        // vel. de transmissão
+    radio.setChannel(netw_channel);       // canal de transmissão
+    // Abertura do canal de comunicação:
+    radio.openWritingPipe(canais[1]);     // transmito no canal 1 
+    // radio.openReadingPipe(1, canais[0]);  // recebo do canal 0
+
+    data.pwm_A = 50;    data.pwm_B = 50;
+}
+/*
+void traduz_str(String ent){
+    String aux;
+    char res[2];
+    char chr;
+    long t1;
+    uint16_t t2;
+    int i = 0, j = 0;
+    String *teste;
+
+    chr = ent[0];
+//    Serial.print("Char: ");
+//    Serial.println(chr);
+    while (true){
+        if (ent[i] == ' ' || ent[i] == '!'){
+            Serial.println(" 'qreba'");
+            break;
+        }
+        else{
+            res[i] = ent[i];
+            Serial.print(ent[i]);
+            i++;
+        }
+    }
+    Serial.println(aux);
+    t1 = atoi(res);
+    Serial.print("Id: ");
+    Serial.println(t1);
+}
+*/
+void traduz_str(String ent){
+    char subs[6][6];
+
+    int j, i = 0, k = 0;
+  
+    while(ent[i] != '\0') {
+        j = 0;
+        while ((ent[i] != ' ') && (ent[i] != '\0')) {
+            subs[k][j] = ent[i];
+            i++;
+            j++;
+        }
+        subs[k][j] = '\0';
+        Serial.println(subs[k]);
+        i++;
+        k++;
+    }
+    
+ 
+}
+
+
+void loop() {
+//
+//    unsigned long started_waiting_at = millis();
+//    if (Serial.available()/* && (millis() - started_waiting_at < 100 )*/){
+//        data_input = Serial.read();
+//        final_time = millis();
+//    }
+//    if (final_time - started_waiting_at > 3000 )      // se o usuário demorar p/ mandar um dado, decido deixar o robô parado
+//        data_input = 'x';
+//    translate_input(data_input);
+//    Serial.println(data.hexa,HEX);
+//    send_message();
+//    // if (data == 'l')
+//    //     digitalWrite (L1G, HIGH);    // este é um teste simples: ligo o LED caso a entrada seja l
+    String v;
+    if (Serial.available()){
+      v = Serial.readString();
+      Serial.println(v);
+      traduz_str(v);    
+    }
+    else
+      Serial.println("Não funfou");
+
+
+    delay(500);
 }
